da0439ff1cce6a91ca92b959138f7c571aa0b44d
Vinicius Campos <vcampos.pitangui@gmail.com>
2019-10-23 14:11:31 -0300

    Add my_select method

--- enumerable.rb
+++ enumerable.rb
@@ -20,8 +20,20 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_select
+    return self.to_enum if !block_given?
+    ret_arr = []
+    self.my_each do |i|
+      if(yield(i))
+        ret_arr.push(i)
+      end
+    end
+    return ret_arr
+  end
 end
 
-puts (1..10).my_each_with_index do |x, i|
-  puts x.to_s + ' ' + i.to_s
-end
+
+arr =  (0..10).my_select { |x| x > 3}
+
+puts arr
