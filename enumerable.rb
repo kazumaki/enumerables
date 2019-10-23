90bb82146334d8797deae2c53e58f1eacd3b96c5
Vinicius Campos <vcampos.pitangui@gmail.com>
2019-10-23 14:06:40 -0300

    Add my_each_with_index method

--- enumerable.rb
+++ enumerable.rb
@@ -1,12 +1,27 @@
 module Enumerable
   def my_each
     return self.to_enum if !block_given?
-    range_arr = self.to_a
 
+    arr = self.to_a
     i = 0
-    while (i < range_arr.length) do
-      yield(range_arr[i])
+    while (i < arr.length) do
+      yield(arr[i])
       i += 1
     end  
   end
+
+  def my_each_with_index
+    return self.to_enum if !block_given?
+
+    arr = self.to_a
+    i = 0
+    while (i < arr.length) do
+      yield(arr[i], i)
+      i += 1
+    end
+  end
+end
+
+puts (1..10).my_each_with_index do |x, i|
+  puts x.to_s + ' ' + i.to_s
 end
